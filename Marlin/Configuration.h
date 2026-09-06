e21eac18c0f934d916e4dff249e3399ae8496205
brewdium <spaxcell@gmail.com>
2026-09-06 19:45:52 +0930

    disable classic jerk because it seems to make the printer pause occasionally

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1412,7 +1412,7 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-//#define CLASSIC_JERK
+#define CLASSIC_JERK
 #if ENABLED(CLASSIC_JERK)
   #define DEFAULT_XJERK 10.0
   #define DEFAULT_YJERK 10.0
@@ -1982,7 +1982,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 300
+#define Z_MAX_POS 350
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
